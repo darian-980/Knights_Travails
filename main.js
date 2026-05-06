99e3f9a3aea19e1b6361f256cafb43b44647ec26
Devolas <darian.dihal@gmail.com>
2026-05-05 20:58:39 -0400

    implemented node system where current moves get coordinate and parent values. at the end the final node gets retraced by calling each parent node recursively. already visited moves are appended to visitedArray and cannot be calculated as a future move. BFS so that the first node of each path is checked before continuing

--- main.js
+++ main.js
@@ -4,22 +4,39 @@ function knightMoves(moveCoordinates, endCoordinates) {
 
     function calculate_moves(moveCoordinates, endCoordinates) {
 
+        function createNode() {
+            let coordinate = null;
+            let parent = null;
+
+            return { coordinate, parent }
+        }
+
         const queue = [moveCoordinates];
+        const parentQueue = [];
 
         while (queue.length > 0) {
-            const currentNode = queue.shift();
+            let currentNode = createNode();
+            currentNode.coordinate = queue.shift();
+            if (parentQueue.length > 0) {
+                currentNode.parent = parentQueue.shift();
+            }
             visitedArray.push(currentNode);
 
-            const possibleMoves = addFunction(currentNode);
+            const possibleMoves = addFunction(currentNode.coordinate);
 
             for (let i = 0; i < possibleMoves.length; i++) {
-                console.log(possibleMoves[i])
+                // console.log(possibleMoves[i])
                 if (endCoordinates[0] === possibleMoves[i][0] && endCoordinates[1] === possibleMoves[i][1]) {
-                    console.log("node found")
+                    // console.log("node found")
+                    let finalNode = createNode();
+                    finalNode.coordinate = endCoordinates;
+                    finalNode.parent = currentNode;
+                    retraceMoves(finalNode);
                     return true;
                 } else {
-                    console.log(possibleMoves[i] + " move isn't destination, pushing to queue")
+                    // console.log(possibleMoves[i] + " move isn't destination, pushing to queue")
                     queue.push(possibleMoves[i]);
+                    parentQueue.push(currentNode);
                 }
             }
         }
@@ -49,15 +66,43 @@ function knightMoves(moveCoordinates, endCoordinates) {
 
     function checkVisited(coordinates) {
         for (let i = 0; i < visitedArray.length; i++) {
-            if (coordinates[0] === visitedArray[i][0] && coordinates[1] === visitedArray[i][1]) {
-                console.log("node already visited")
+            if (coordinates[0] === visitedArray[i].coordinate[0] && coordinates[1] === visitedArray[i].coordinate[1]) {
+                // console.log("node already visited")
                 return true;
             }
         }
         return false;
     }
+
+    function retraceMoves(endNode) {
+        const pathArray = [];
+
+        function createPathArray(endNode) {
+            if (endNode.parent !== null) {
+                // console.log("retracing: " + endNode.coordinate)
+                pathArray.push(endNode.coordinate)
+                createPathArray(endNode.parent);
+            }
+            else {
+                // console.log("retracing: " + endNode.coordinate)
+                pathArray.push(endNode.coordinate)
+                // console.log(pathArray)
+            }
+        }      
+
+        createPathArray(endNode);
+
+        const backwardsArray = pathArray.reverse(); // iterate through array backwards because it is appended to the array from end -> start
+
+        console.log("You made it in " + backwardsArray.length + " moves! Here's your path:");
+        for (let i = 0; i < backwardsArray.length; i++) {
+            console.log("[" + backwardsArray[i][0] + ", " + backwardsArray[i][1] + "]")
+        }
+    }
 }
 
 
-console.log(knightMoves([3, 3], [4, 3]))
+knightMoves([3, 3], [4, 3])
+knightMoves([3, 3], [6, 6])
+knightMoves([1, 1], [7, 6])
 //
